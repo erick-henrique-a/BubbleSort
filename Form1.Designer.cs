e4a6d330ecb6ba3e2962d6dcf1c577c8c6f49c0d
Erick Henrique <71470639+erick-henrique-a@users.noreply.github.com>
2024-04-09 19:18:44 -0300

    Dump do projeto
    Arquivamento de antigo projeto no Github

---
+++ Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace trabalho
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.medio_btn = new System.Windows.Forms.Button();
+            this.pior_btn = new System.Windows.Forms.Button();
+            this.melhor_btn = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // medio_btn
+            // 
+            this.medio_btn.Location = new System.Drawing.Point(581, 138);
+            this.medio_btn.Name = "medio_btn";
+            this.medio_btn.Size = new System.Drawing.Size(177, 58);
+            this.medio_btn.TabIndex = 0;
+            this.medio_btn.Text = "medio";
+            this.medio_btn.UseVisualStyleBackColor = true;
+            this.medio_btn.Click += new System.EventHandler(this.medio_Click);
+            // 
+            // pior_btn
+            // 
+            this.pior_btn.Location = new System.Drawing.Point(581, 202);
+            this.pior_btn.Name = "pior_btn";
+            this.pior_btn.Size = new System.Drawing.Size(177, 46);
+            this.pior_btn.TabIndex = 2;
+            this.pior_btn.Text = "pior";
+            this.pior_btn.UseVisualStyleBackColor = true;
+            this.pior_btn.Click += new System.EventHandler(this.pior_Click);
+            // 
+            // melhor_btn
+            // 
+            this.melhor_btn.Location = new System.Drawing.Point(581, 76);
+            this.melhor_btn.Name = "melhor_btn";
+            this.melhor_btn.Size = new System.Drawing.Size(177, 56);
+            this.melhor_btn.TabIndex = 3;
+            this.melhor_btn.Text = "melhor";
+            this.melhor_btn.UseVisualStyleBackColor = true;
+            this.melhor_btn.Click += new System.EventHandler(this.melhor_click);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(27, 48);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(493, 300);
+            this.chart1.TabIndex = 4;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.melhor_btn);
+            this.Controls.Add(this.pior_btn);
+            this.Controls.Add(this.medio_btn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button medio_btn;
+        private System.Windows.Forms.Button pior_btn;
+        private System.Windows.Forms.Button melhor_btn;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace trabalho
+{
+    public partial class Form1 : Form
+    {
+        Stopwatch timer;
+        int[] vetor = new int[10000];
+        Random ale = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            chart1.Series.Clear();
+
+            chart1.Series.Add("Não Otimizado");
+            chart1.Series[0].ChartType = SeriesChartType.Line;
+
+            chart1.Series.Add("Otimizado");
+            chart1.Series[1].ChartType = SeriesChartType.Line;
+
+            chart1.Series.Add("Complexidade");
+            chart1.Series[2].ChartType = SeriesChartType.Line;
+
+            for (int i = 0; i < vetor.Length; i++)
+            {
+                vetor[i] = ale.Next(20000);
+            }
+        }
+        public Stopwatch bubblesortruim(int[] array)
+ {
+            int temp;
+            int[] ord = new int[array.Length];
+            array.CopyTo(ord, 0);
+            timer = new Stopwatch();
+            timer.Start();
+            for (int j = 0; j < ord.Length; j++)
+        {
+            for (int i = 0; i < ord.Length-1; i++)
+            {
+                if (ord[i] > ord[i + 1])
+                {
+                    temp = ord[i + 1];
+                    ord[i + 1] = ord[i];
+                    ord[i] = temp;
+                }
+            }
+            }
+            timer.Stop();
+        return timer;
+}
+        public Stopwatch bubblesortbom(int[] array)
+        {
+            int temp;
+            bool boo;
+            int[] ord = new int[array.Length];
+            array.CopyTo(ord, 0);
+            timer = new Stopwatch();
+            timer.Start();
+            for (int j = 0; j < ord.Length; j++)
+            {
+                boo = true;
+                for (int i = 0; i < ord.Length - i - 1; i++)
+                {
+                    if (ord[i] > ord[i + 1])
+                    {
+                        boo = false;
+                        temp = ord[i + 1];
+                        ord[i + 1] = ord[i];
+                        ord[i] = temp;
+                    }
+                }
+                if(boo == true)
+                {
+                    break;
+                }
+            }
+            timer.Stop();
+            return timer;
+        }
+        private void medio_Click(object sender, EventArgs e)
+        {
+            chart1.Series[0].Points.Clear();
+            chart1.Series[1].Points.Clear();
+            double constante=0;
+            int count=0;
+            for (int i = 0; i <= 10000; i = i + 200, count++)
+            {
+                int[] fra = new int[i];
+                Array.Copy(vetor, 0, fra, 0, i);//copia de um pro outro do indice 0 para o indice 0, o i e o limite
+                Stopwatch bom = bubblesortbom(fra);
+                Stopwatch ruim = bubblesortruim(fra);
+                chart1.Series[0].Points.Add((int)ruim.ElapsedMilliseconds);
+                chart1.Series[1].Points.Add((int)bom.ElapsedMilliseconds);
+                while ((count * count * constante < (double)ruim.ElapsedMilliseconds) && (count * count * constante < (double)ruim.ElapsedMilliseconds))
+                {
+                    constante += 0.000001;
+                }
+            }
+            for (int i = 0; i < chart1.Series[0].Points.Count; i++)
+            {
+                chart1.Series[2].Points.Add(i * i * constante);
+            }
+        }
+
+        private void pior_Click(object sender, EventArgs e)
+        {
+            int[] pior = new int[vetor.Length];
+            for(int i = 0; i < 10000; i++)
+            {
+                pior[i] = 10000-i;
+            }
+            chart1.Series[0].Points.Clear();
+            chart1.Series[1].Points.Clear();
+            double constante = 0;
+            int count = 0;
+            for (int i = 0; i <= 10000; i = i + 200, count++)
+            {
+                int[] fra = new int[i];
+                Array.Copy(pior, 0, fra, 0, i);//copia de um pro outro do indice 0 para o indice 0, o i e o limite
+                Stopwatch bom = bubblesortbom(fra);
+                Stopwatch ruim = bubblesortruim(fra);
+                chart1.Series[0].Points.Add((int)ruim.ElapsedMilliseconds);
+                chart1.Series[1].Points.Add((int)bom.ElapsedMilliseconds);
+                while ((count * count * constante < (double)ruim.ElapsedMilliseconds) && (count * count * constante < (double)ruim.ElapsedMilliseconds))
+                {
+                    constante += 0.000001;
+                }
+            }
+            for (int i = 0; i < chart1.Series[0].Points.Count; i++)
+            {
+                chart1.Series[2].Points.Add(i * i * constante);
+            }
+        }
+
+        private void melhor_click(object sender, EventArgs e)
+        {
+            int[] melhor = new int[vetor.Length];
+            for(int i = 0; i < 10000; i++)
+            {
+                melhor[i] = i;
+            }
+            chart1.Series[0].Points.Clear();
+            chart1.Series[1].Points.Clear();
+            double constante = 0;
+            int count = 0;
+            for (int i = 0; i <= 10000; i = i + 200, count++)
+            {
+                int[] fra = new int[i];
+                Array.Copy(melhor, 0, fra, 0, i);//copia de um pro outro do indice 0 para o indice 0, o i e o limite
+                Stopwatch bom = bubblesortbom(fra);
+                Stopwatch ruim = bubblesortruim(fra);
+                chart1.Series[0].Points.Add((int)ruim.ElapsedMilliseconds);
+                chart1.Series[1].Points.Add((int)bom.ElapsedMilliseconds);
+                while ((count * count * constante < (double)ruim.ElapsedMilliseconds) && (count * count * constante < (double)ruim.ElapsedMilliseconds))
+                {
+                    constante += 0.000001;
+                }
+            }
+            for (int i = 0; i < chart1.Series[0].Points.Count; i++)
+            {
+                chart1.Series[2].Points.Add(i * i * constante);
+            }
+        }
+    }
+}
